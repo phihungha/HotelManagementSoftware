8180bb8743dcead759ba145476839ee949ba671f
haphihung <haphihung55@gmail.com>
2022-05-12 21:13:43 +0700

    Initial setup for dependency injection

--- HotelManagementSoftware/App.xaml.cs
+++ HotelManagementSoftware/App.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using HotelManagementSoftware.Data;
+using HotelManagementSoftware.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Toolkit.Mvvm.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +18,24 @@ namespace HotelManagementSoftware
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            Services = ConfigureServices();
+            InitializeComponent();
+        }
+
+        public new static App Current => (App)Application.Current;
+
+        public IServiceProvider Services { get; }
+
+        /// <summary>
+        /// Configures the services for the application.
+        /// </summary>
+        private static IServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            return services.BuildServiceProvider();
+        }
     }
 }
