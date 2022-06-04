b2657d61e4b79674a13175d3007063addd02ab4c
LeQT8522 <105804775+LeQT8522@users.noreply.github.com>
2022-06-04 19:54:28 +0700

    room list

--- HotelManagementSoftware/ViewModels/RoomsVM.cs
+++ HotelManagementSoftware/ViewModels/RoomsVM.cs
@@ -1,13 +1,43 @@
-﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+﻿using HandyControl.Controls;
+using HotelManagementSoftware.Data;
+using Microsoft.Toolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Windows.Input;
+
 namespace HotelManagementSoftware.ViewModels
 {
-    public class RoomsVM : ObservableValidator
+    public class RoomsVM
     {
+        public ObservableCollection<Room> Rooms { get; set; }
+        public RoomsVM()
+        {
+            Rooms = new ObservableCollection<Room>();
+            CommandAdd = new RelayCommand(executeAddAction);
+            CommandDelete = new RelayCommand(executeDeleteAction);
+            CommandEdit = new RelayCommand(executeEditAction);
+        }
+        #region command
+        public ICommand CommandAdd { get; }
+        public ICommand CommandDelete { get; }
+        public ICommand CommandEdit { get; }
+        public void executeAddAction()
+        {
+            MessageBox.Show("Add");
+        }
+        public void executeDeleteAction()
+        {
+            MessageBox.Show("Delete");
+        }
+        public void executeEditAction()
+        {
+            MessageBox.Show("Edit");
+        }
+        #endregion
     }
 }
