acfef7707ab1d67d52b3b50b1f15e828313fe5ba
LeQT8522 <105804775+LeQT8522@users.noreply.github.com>
2022-06-04 13:59:47 +0700

    room type list

--- HotelManagementSoftware/ViewModels/RoomTypesVM.cs
+++ HotelManagementSoftware/ViewModels/RoomTypesVM.cs
@@ -1,13 +1,47 @@
-﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using HotelManagementSoftware.Data;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+using Microsoft.Toolkit.Mvvm.Input;
+using HandyControl.Controls;
 
 namespace HotelManagementSoftware.ViewModels
 {
-    public class RoomTypesVM : ObservableValidator
+    public class RoomTypesVM
     {
+        public ObservableCollection<RoomType> RoomTypes { get; set; }
+
+        public RoomTypesVM()
+        {
+            RoomTypes = new ObservableCollection<RoomType>();
+            CommandAdd = new RelayCommand(executeAddAction);
+            CommandDelete = new RelayCommand(executeDeleteAction);
+            CommandEdit = new RelayCommand(executeEditAction);
+        }
+
+        #region command
+        public ICommand CommandAdd { get; }
+        public ICommand CommandDelete { get; }
+        public ICommand CommandEdit { get; }
+        public void executeAddAction()
+        {
+            MessageBox.Show("Add");
+        }
+        public void executeDeleteAction()
+        {
+            MessageBox.Show("Delete");
+        }
+        public void executeEditAction()
+        {
+            MessageBox.Show("Edit");
+        }
+        #endregion
     }
 }
