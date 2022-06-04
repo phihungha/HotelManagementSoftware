cfe1be3943fe7a54efb10c4914928c17ae15cb7b
Quang Vu <ngqangvvu@gmail.com>
2022-06-04 19:34:12 +0700

    Add departures list

--- HotelManagementSoftware/ViewModels/DeparturesVM.cs
+++ HotelManagementSoftware/ViewModels/DeparturesVM.cs
@@ -4,10 +4,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using HotelManagementSoftware.Data;
 
 namespace HotelManagementSoftware.ViewModels
 {
     public class DeparturesVM : ObservableValidator
     {
+        public ObservableCollection<Reservation> Departure { get; set; }
+
+        public DeparturesVM()
+        {
+            Departure = new ObservableCollection<Reservation>();
+            addArrival();
+        }
+        private void addArrival()
+        {
+            Departure.Add(new Reservation(
+                   new DateTime(2020, 2, 13), new DateTime(2020, 2, 15), 2, ReservationStatus.Reserved
+                   ));
+        }
+
+       
     }
 }
