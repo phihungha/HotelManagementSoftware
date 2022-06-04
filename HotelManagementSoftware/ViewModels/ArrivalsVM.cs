3f64e33a76953fc8732772832be49b3f2867ce54
Quang Vu <ngqangvvu@gmail.com>
2022-06-04 19:20:09 +0700

    Add Arrivals list

--- HotelManagementSoftware/ViewModels/ArrivalsVM.cs
+++ HotelManagementSoftware/ViewModels/ArrivalsVM.cs
@@ -4,10 +4,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using HotelManagementSoftware.Data;
+
 
 namespace HotelManagementSoftware.ViewModels
 {
     public class ArrivalsVM : ObservableValidator
     {
+        public ObservableCollection<Reservation> Arrival { get; set; }
+
+        public ArrivalsVM()
+        {
+            Arrival = new ObservableCollection<Reservation>();
+            addArrival();
+        }
+        private void addArrival()
+        {
+            Arrival.Add(new Reservation(
+                   new DateTime(2020, 2, 13), new DateTime(2020, 2, 15), 2, ReservationStatus.Reserved 
+                   ));
+        }
     }
 }
